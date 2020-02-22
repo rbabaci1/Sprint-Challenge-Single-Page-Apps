e0f90f4a62ee04e98bd1f6199e7e09b716daa937
rabah babaci <4rabah@gmail.com>
2020-02-22 10:39:20 -0800

    create a card for every character using reactstrap

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -11,5 +11,20 @@ import {
 } from "reactstrap";
 
 export default function CharacterCard({ character }) {
-  return <span>todo: character</span>;
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src={character.image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </CardText>
+          {/* <Button>Button</Button> */}
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
