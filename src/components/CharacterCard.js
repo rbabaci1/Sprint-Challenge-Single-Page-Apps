012f4207721b7380f1cbd1802bfcf24f660817d4
rabah babaci <4rabah@gmail.com>
2020-02-22 10:56:07 -0800

    finish styling character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -16,12 +16,22 @@ export default function CharacterCard({ character }) {
       <Card>
         <CardImg top width="100%" src={character.image} alt="Card image cap" />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
+          <CardTitle>
+            <h1>{character.name}</h1>
+          </CardTitle>
+          <CardSubtitle>
+            <span>Status: </span>
+            <p>{character.status}</p>
+          </CardSubtitle>
+          <CardSubtitle>
+            <span>Gender: </span>
+            <p>{character.gender}</p>
+          </CardSubtitle>
+          <CardSubtitle>
+            <span>Number of episodes: </span>
+            <p>{character.episode.length}</p>
+          </CardSubtitle>
+
           {/* <Button>Button</Button> */}
         </CardBody>
       </Card>
