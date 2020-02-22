3dc3673d653608c10125ccd942b835a4011d2885
rabah babaci <4rabah@gmail.com>
2020-02-22 09:32:01 -0800

    mover all main routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
 
 export default function App() {
   return (
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
-
-import CharacterList from "./CharacterList";
+import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
   return (
