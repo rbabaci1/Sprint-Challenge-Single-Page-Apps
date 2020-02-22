4066f81eaa6c02efc138f40fecc8c8a061df46ec
rabah babaci <4rabah@gmail.com>
2020-02-22 10:23:13 -0800

    import all needed elements from bootsrap

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+
 export default function CharacterCard({ character }) {
   return <span>todo: character</span>;
 }
