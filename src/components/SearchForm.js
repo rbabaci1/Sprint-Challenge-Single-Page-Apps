ce69a6c39709adb7bdf2504b838c73482c3ee482
rabah babaci <4rabah@gmail.com>
2020-02-22 11:01:15 -0800

    move the created state to search form component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+  const [searchResult, setSearchResult] = useState([]);
+
+  return <section className="search-form">// Add a search form here</section>;
 }
